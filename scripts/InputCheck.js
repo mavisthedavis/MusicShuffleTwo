5a10ce577092a7a4f0b47c56b3763fe7e36af207
mavisthedavis <mavisledavis563@gmail.com>
2026-02-11 13:26:13 -0600

    Add files via upload

---
+++ scripts/InputCheck.js
@@ -0,0 +1,35 @@
+var inputIndex = 0;  
+audioList = [];  
+playing = false;  
+playingIndex = 0; 
+var filesAdded = false; 
+const input = $('#file-input');  
+input.on('change', handleSubmit);   
+function handleSubmit(event) {   
+    filesAdded = true; 
+    for (let x = 0; x < input.prop('files').length; x++) {   
+        /*
+        var audioObj = { 
+            audio: new Audio(URL.createObjectURL(input.prop('files')[x])),  
+            index: inputIndex,
+        };  
+        */   
+        var audioElem = $("<audio></audio>"); 
+        audioElem.attr("src", URL.createObjectURL(input.prop('files')[x]));
+        var audioObj = { 
+            audio: audioElem,  
+            index: inputIndex,
+        };  
+        audioList.push(audioObj);   
+        audioList[inputIndex].audio.prop('controls', true);;    
+        //console.log(audioList[inputIndex].audio); 
+        audioList[inputIndex].audio.on("play", checkPlay);  
+        audioList[inputIndex].audio.on("pause", checkPause); 
+
+        $("body").append(audioList[inputIndex].audio); 
+        inputIndex++; 
+    }  
+    setInterval(checkEnd, 1);  
+    //audioList[i].addEventListener('onended', audioEnd());
+
+}
---
+++ scripts/Visualiser.js
@@ -0,0 +1,27 @@
+//import { defineConfig } from 'vitest/config';
+var btn = document.getElementById("mybtn");  
+var audio = document.querySelector("audio"); 
+var audioCtx = new window.AudioContext(); 
+console.log(audioCtx.state); 
+document.addEventListener('click', () => {
+  // 3. Resume the context on the user gesture 
+  console.log("check"); 
+  if (audioCtx.state === 'suspended') {
+    audioCtx.resume(); 
+    console.log(audioCtx.destination); 
+  }
+}, { once: true });
+audio.onloadedmetadata = (e) => {
+    audio.play();
+    audioCtx.resume();
+    console.log(audioCtx.state === 'suspended');  
+    const track = audioCtx.createMediaElementSource(audio); 
+    console.log(track); 
+    setInterval(checkCon, 1); 
+
+}; 
+
+function checkCon() {  
+    //console.log(audioCtx.state); 
+    console.log(audioCtx.destination); 
+}
