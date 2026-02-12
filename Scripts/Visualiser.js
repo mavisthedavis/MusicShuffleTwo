1847cc86cb08d92db60c7d534d1f9eba0c86980e
mavisthedavis <mavisledavis563@gmail.com>
2026-02-12 09:18:42 -0600

    Update Visualiser.js

--- Scripts/Visualiser.js
+++ Scripts/Visualiser.js
@@ -1,24 +1,41 @@
-//import { defineConfig } from 'vitest/config';
-var btn = document.getElementById("mybtn");  
-var audio = document.querySelector("audio"); 
-var audioCtx = new window.AudioContext(); 
-document.addEventListener('click', () => {
-  // 3. Resume the context on the user gesture 
-  console.log("check"); 
-  if (audioCtx.state === 'suspended') {
-    audioCtx.resume(); 
-  }
-}, { once: true });
-audio.onloadedmetadata = (e) => {
-    audioCtx.resume();
-    console.log(audioCtx.state === 'suspended');  
-    const track = audioCtx.createMediaElementSource(audio); 
-    console.log(track); 
-    setInterval(checkCon, 1); 
-
-}; 
-
-function checkCon() {  
-    //console.log(audioCtx.state); 
-    $("#console").text(audioCtx.destination);  
+if (typeof Uint8Array !== 'undefined') {
+    console.log("Uint8Array is supported!");
+    // Example usage:
+    const arr = new Uint8Array(2);
+    console.log(arr);
+} else {
+    console.log("Uint8Array is NOT supported.");
+}   
+$("mybtn").css("width", "10px");
+const audioCtx = new AudioContext(); 
+console.log(audioCtx.state); 
+var audio = new Audio("https://codehs.com/uploads/1ca205fa48224105f9196c2f16bbe828"); 
+audio.controls = true; 
+audio.autoplay = true;
+audio.preload = "auto";  
+audio.setAttribute("controls", true);
+audio.setAttribute('crossOrigin', "anonymous");
+audio.setAttribute('autoplay', true);
+audio.setAttribute('preload', "auto");
+const audioSourceNode = audioCtx.createMediaElementSource(audio);    
+const analyser = audioCtx.createAnalyser();   
+analyser.fftSize = 2048;
+const bufferLength = analyser.frequencyBinCount;
+var myDataArray = new Float32Array(bufferLength);
+audioSourceNode.connect(analyser); 
+analyser.connect(audioCtx.destination);  
+document.body.appendChild(audio);   
+audio.play();  
+play(); 
+function check() { 
+    analyser.getFloatFrequencyData(myDataArray);   
+    for (let i = 0; i < myDataArray.length; i++) {  
+        $("mybtn").text(myDataArray[i]); 
+        //console.log(myDataArray[i]); 
+    }
+    
+}   
+ 
+function play() {  
+    setInterval(check, 500);  
 }
