ca9ba7c6cfdd212e241b7a24d6ee497536cb7245
mavisthedavis <mavisledavis563@gmail.com>
2026-02-12 13:04:39 -0600

    Update Visualiser.js

--- Scripts/Visualiser.js
+++ Scripts/Visualiser.js
@@ -1,22 +1,10 @@
-if (typeof Uint8Array !== 'undefined') {
-    console.log("Uint8Array is supported!");
-    // Example usage:
-    const arr = new Uint8Array(2);
-    console.log(arr);
-} else {
-    console.log("Uint8Array is NOT supported.");
-}   
-$("mybtn").css("width", "10px");
 const audioCtx = new AudioContext(); 
-console.log(audioCtx.state); 
+var divList = []; 
 var audio = new Audio("https://codehs.com/uploads/1ca205fa48224105f9196c2f16bbe828"); 
-audio.controls = true; 
-audio.autoplay = true;
-audio.preload = "auto";  
-audio.setAttribute("controls", true);
-audio.setAttribute('crossOrigin', "anonymous");
-audio.setAttribute('autoplay', true);
-audio.setAttribute('preload', "auto");
+audio.setAttribute("controls", true); //don't need this
+audio.setAttribute('crossOrigin', "anonymous"); // need this
+audio.setAttribute('autoplay', true); // won't need this when the time comes 
+audio.setAttribute('preload', "auto"); //or this
 const audioSourceNode = audioCtx.createMediaElementSource(audio);    
 const analyser = audioCtx.createAnalyser();   
 analyser.fftSize = 2048;
@@ -26,16 +14,35 @@ audioSourceNode.connect(analyser);
 analyser.connect(audioCtx.destination);  
 document.body.appendChild(audio);   
 audio.play();  
-play(); 
+console.log(myDataArray.length); 
+play();  
 function check() {  
-    analyser.getFloatFrequencyData(myDataArray);   
-    for (let i = 0; i < myDataArray.length; i++) {  
-        $("#mybtn").css("width", (myDataArray[i] * -1) + "px"); 
+    console.log(myDataArray); 
+    analyser.getFloatFrequencyData(myDataArray);  
+    var average = 0; 
+    for (let i = 0; i < myDataArray.length; i++) { 
+        average += myDataArray[i];      
+    } 
+    average /= myDataArray.length;  
+    for (let i = 0; i < myDataArray.length; i++) {   
+        if (audio.paused != true) { 
+            divList[i].css("width", (myDataArray[i] * -1) + "px");  
+        } 
         //console.log(myDataArray[i]); 
     }
     
-}   
- 
-function play() {  
-    setInterval(check, 300);  
+}  
+function play() {   
+    for (let i = 0; i < myDataArray.length; i++) {  
+        setUpVis(); 
+    }
+    setInterval(check, 100);  
+}
+function setUpVis() { 
+    var div = $("<div></div>");  
+    div.css("background-color", "red"); 
+    div.css("height", 100/myDataArray.length + "vh"); 
+    div.css("width", "10px");  
+    divList.push(div); 
+    $("body").append(div); 
 }
