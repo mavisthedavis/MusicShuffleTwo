2f9755adaca52b7d93e5cc75705bea8183854808
mavisthedavis <mavisledavis563@gmail.com>
2026-02-18 13:02:04 -0600

    Update Visualiser.js

--- Scripts/Visualiser.js
+++ Scripts/Visualiser.js
@@ -1,48 +1,65 @@
 const audioCtx = new AudioContext(); 
 var divList = []; 
-var audio = new Audio("https://codehs.com/uploads/1ca205fa48224105f9196c2f16bbe828"); 
-audio.setAttribute("controls", true); //don't need this
-audio.setAttribute('crossOrigin', "anonymous"); // need this
-audio.setAttribute('autoplay', true); // won't need this when the time comes 
-audio.setAttribute('preload', "auto"); //or this
-const audioSourceNode = audioCtx.createMediaElementSource(audio);    
+//var audio = new Audio("https://codehs.com/uploads/1ca205fa48224105f9196c2f16bbe828"); 
+//audio.setAttribute("controls", true); //don't need this
+//audio.setAttribute('crossOrigin', "anonymous"); // need this
+//audio.setAttribute('autoplay', true); // won't need this when the time comes 
+//audio.setAttribute('preload', "auto"); //or this 
 const analyser = audioCtx.createAnalyser();   
-analyser.fftSize = 2048;
-const bufferLength = analyser.frequencyBinCount;
-var myDataArray = new Float32Array(bufferLength);
-audioSourceNode.connect(analyser); 
-analyser.connect(audioCtx.destination);  
-document.body.appendChild(audio);   
-audio.play();  
-console.log(myDataArray.length); 
-play();  
+var audioSourceNode;  
+var nodeList = [];  
+function initAnaly() {   
+    /*
+    audioSourceNode = audioCtx.createMediaElementSource(audioList[playingIndex].audio[0]);    
+    
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    myDataArray = new Float32Array(bufferLength);
+    audioSourceNode.connect(analyser); 
+    analyser.connect(audioCtx.destination);    
+    //}   
+    //nodeList.push(audioSourceNode);  
+    */
+    
+}
+
+
+
+
 function check() {  
-    console.log(myDataArray); 
     analyser.getFloatFrequencyData(myDataArray);  
     var average = 0; 
     for (let i = 0; i < myDataArray.length; i++) { 
         average += myDataArray[i];      
     } 
-    average /= myDataArray.length;  
+    average /= myDataArray.length;   
+    //console.log(divList.length); 
     for (let i = 0; i < myDataArray.length; i++) {   
-        if (audio.paused != true) { 
+        if (audioList[playingIndex].audio[0].paused != true) {  
             divList[i].css("width", (myDataArray[i] * -1) + "px");  
         } 
         //console.log(myDataArray[i]); 
     }
     
 }  
-function play() {   
-    for (let i = 0; i < myDataArray.length; i++) {  
-        setUpVis(); 
-    }
-    setInterval(check, 100);  
+function Aplaying() {  
+    removeVis();  
+    setUpVis();
+    initAnaly(); 
 }
-function setUpVis() { 
-    var div = $("<div></div>");  
-    div.css("background-color", "red"); 
-    div.css("height", 100/myDataArray.length + "vh"); 
-    div.css("width", "10px");  
-    divList.push(div); 
-    $("body").append(div); 
+function setUpVis() {   
+    for (let i = 0; i < myDataArray.length; i++) {  
+        var div = $("<div></div>");  
+        div.css("background-color", "red"); 
+        div.css("height", 100/myDataArray.length + "vh"); 
+        div.css("width", "10px");  
+        divList.push(div); 
+        $("body").append(div);  
+    }
+} 
+function removeVis() {  
+    for (let i = 0; i < divList.length; i++) { 
+        divList[i].remove();     
+    } 
+    divList = [];   
 }
