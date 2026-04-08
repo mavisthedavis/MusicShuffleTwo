370c1d0426e3214108ce4bcbbe62cf0abc94dfe4
Mavis Davis <mavisledavis563@gmail.com>
2026-04-08 13:19:03 -0500

    Refactor line interaction functions in inputs.js

--- Scripts/inputs.js
+++ Scripts/inputs.js
@@ -1,10 +1,40 @@
-//$(".audioElm > div > div > div").on( "mousedown", moveLine);  
-moveLine(); 
-function moveLine() { 
-    //$(this).offset().left);  
-    $(this).css({
-      left: 100
-    }); 
-    console.log(this); 
+$("#main-line").on( "mousedown", linePressed);  
+//$("#main-line").on("mouseup", lineUnPressed);  
+//$("html").on("mouseup", linePressed);
+$("html").on("mouseup", lineUnPressed); 
+var isPressed = false;  
+var element = null; 
+function linePressed() { 
+    isPressed = true;   
+    element = this;      
+    //console.log(FixCon($(element).css("width")) / 2.7 * (FixCon($(element).css("left"))/audioList[playingIndex].audio[0].duration)); 
+    //console.log(audioList[playingIndex].audio[0].currentTime + "CT");
     
+
+}   
+function lineUnPressed() {   
+    isPressed = false;  
+    if (element == $("#main-line")[0]) {   
+        audioList[playingIndex].audio[0].currentTime = FixCon($(element).css("width")) / 5.2 * (FixCon($(element).css("left"))/audioList[playingIndex].audio[0].duration); 
+        //audioList[playingIndex].audio[0].currentTime = 5.87 * (FixCon($(element).css("left"))/audioList[playingIndex].audio[0].duration); 
+    } else if (checkForElement()) {  
+        for (let i = 0; i < audioList.length; i++) {  
+            if (elementList[i].children[1].children[1].children[0] == element) { 
+                //audioList[i].audio[0].currentTime = FixCon($(element).css("left")) * audioList[i].audio[0].duration/FixCon($(".audioElm > div > div").css("width"));  
+                audioList[i].audio[0].currentTime = FixCon($(element).css("width")) / 2.7 * (FixCon($(element).css("left"))/audioList[i].audio[0].duration); 
+            } 
+        } 
+    
+    }  
+
+    element = null; 
+    
+} 
+function checkForElement() { 
+    for (let i = 0; i < audioList.length; i++) { 
+        if (element == elementList[i].children[1].children[1].children[0]) { 
+            return true;     
+        }
+    }  
+    return false; 
 }
