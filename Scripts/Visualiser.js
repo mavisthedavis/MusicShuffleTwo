34a13e82678f1c73feef0981732cba9c843237d3
Mavis Davis <mavisledavis563@gmail.com>
2026-04-08 13:15:25 -0500

    Refactor variable declarations and update colors
    Refactor Visualiser.js to improve variable declarations and update visual elements.

--- Scripts/Visualiser.js
+++ Scripts/Visualiser.js
@@ -1,8 +1,12 @@
-var wait = false;  
-var nodeList = []; 
-var divList = []; 
 const audioCtx = new AudioContext(); 
-analyser = audioCtx.createAnalyser();     
+var divList = []; 
+//var audio = new Audio("https://codehs.com/uploads/1ca205fa48224105f9196c2f16bbe828"); 
+//audio.setAttribute("controls", true); //don't need this
+//audio.setAttribute('crossOrigin', "anonymous"); // need this
+//audio.setAttribute('autoplay', true); // won't need this when the time comes 
+//audio.setAttribute('preload', "auto"); //or this 
+const analyser = audioCtx.createAnalyser();   
+var nodeList = [];  
 
 
 
@@ -13,24 +17,25 @@ function check() {
     for (let i = 0; i < myDataArray.length; i++) { 
         average += myDataArray[i];      
     } 
-    average /= myDataArray.length;    
+    average /= myDataArray.length;   
     //console.log(divList.length); 
     for (let i = 0; i < myDataArray.length; i++) {   
         if (audioList[playingIndex].audio[0].paused != true) {  
             divList[i].css("width", (myDataArray[i] * -1) + "px");  
         } 
         //console.log(myDataArray[i]); 
-    }
+    } 
+    updateTime(); 
     
 }  
-function Aplaying() {   
+function Aplaying() {  
     removeVis();  
     setUpVis();
 }
 function setUpVis() {   
     for (let i = 0; i < myDataArray.length; i++) {  
         var div = $("<div></div>");  
-        div.css("background-color", "red"); 
+        div.css("background-color", "black"); 
         div.css("height", 100/myDataArray.length + "vh"); 
         div.css("width", "10px");  
         divList.push(div); 
