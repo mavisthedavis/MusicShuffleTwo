0fe87b6689599d51fcd89c20288e46999a36a6bb
Mavis Davis <mavisledavis563@gmail.com>
2026-04-08 13:16:43 -0500

    Refactor addAudio function and improve element creation

--- Scripts/addElm.js
+++ Scripts/addElm.js
@@ -1,8 +1,13 @@
 var elementList = []; 
 
-addAudio();
-
-function addAudio() {  
+window.onload = function() { 
+    $("#main-line").draggable({ 
+        axis: "x", 
+        containment: "parent", 
+    })    
+}
+function addAudioElm(name) {   
+    console.log(name)
     //create div container
     var audioDiv = new $("<div></div>");  
     audioDiv[0].classList.add("audioElm", "d-flex", "align-items-center"); 
@@ -11,24 +16,37 @@ function addAudio() {
     var button = new $("<button></button>");   
     var image = new $("<img/>");  
     image[0].src = ""; 
-    button[0].classList.add("btn");  
+    button[0].classList.add("btn");   
+    button.on("mousedown", playPause); 
     audioDiv.append(button); 
     button.append(image);  
     //create timeline and timeline children 
     var timeDiv = new $("<div></div>");  
-    var reuseDiv = new $("<div></div>"); 
     var h2 = new $("<h2></h2>");  
-    h2.text("DEFAULT VALUE.mp3");
+    h2.text(name); 
+    
     var childDiv = new $("<div></div>");  
     var grandchildDiv = new $("<div></div>");  
-    var unbornDiv = new $("<div></div>");  
-    $(unbornDiv).draggable({ 
+    $(grandchildDiv).draggable({ 
         axis: "x", 
         containment: "parent", 
     })
     audioDiv.append(timeDiv); 
     timeDiv.append(h2);  
-    timeDiv.append(grandchildDiv); 
-    grandchildDiv.append(unbornDiv);  
+    timeDiv.append(childDiv); 
+    childDiv.append(grandchildDiv);   
+    $(grandchildDiv).on( "mousedown", linePressed);  
+    //$(grandchildDiv).on("mouseup", lineUnPressed);  
+    if (h2.text().length > 43) {  
+        console.log(FixCon(h2.css("font-size")) - (h2.text().length/2)); 
+        h2.css("font-size", FixCon(h2.css("font-size")) * .8 + "px");  
+    }  
+    elementList.push(audioDiv[0]); 
+    console.log(elementList); 
     
+} 
+function FixCon(unit) {  
+    length = unit.length 
+    oldVw = Number(unit.substring(0,length - 2));   
+    return oldVw; 
 }
