971216c262f0a945a82f5d14dcd2c371edc4d089
Mavis Davis <mavisledavis563@gmail.com>
2026-04-14 13:13:53 -0500

    Fix console log formatting for time adjustment calculation

--- Scripts/inputs.js
+++ Scripts/inputs.js
@@ -10,7 +10,7 @@ function linePressed() {
     
     // try dividing eml width by container width
     console.log($(element).css("width") + " Elm width"); 
-    console.log((FixCon($(element).css("width"))/FixCon($("#time").css("width")) + (FixCon($(element).css("left"))/audioList[playingIndex].audio[0].duration)) + " Time Ajust");  
+    console.log(((FixCon($(element).css("width"))/FixCon($("#time").css("width"))) + (FixCon($(element).css("left"))/audioList[playingIndex].audio[0].duration)) + " Time Ajust");  
     console.log($(element).css("left") + " Element position"); 
     console.log(audioList[playingIndex].audio[0].currentTime + " current time of audio");
     //console.log(FixCon($(element).css("width")) / 2.7 * (FixCon($(element).css("left"))/audioList[playingIndex].audio[0].duration)); 
